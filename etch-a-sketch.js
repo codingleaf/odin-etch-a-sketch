7b24fd8d16fce8c9764f1c80c833952a74c65c4d
codingleaf <155798178+codingleaf@users.noreply.github.com>
2025-09-02 04:49:35 +0800

    feat: improve responsiveness

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,3 +1,25 @@
-const GRID_SIZE = 400
+document.addEventListener('DOMContentLoaded', () => {
+  main()
+})
 
+function generateGrid() {
 
+}
+
+function main () {
+  const gridSizeForm = document.getElementById('grid-size')
+  const gridSizeInput = document.getElementById('grid-size-input')
+  gridSizeForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const size = Number(gridSizeInput.value)
+    if (Number.isInteger(size) && size > 0) {
+      generateGrid()
+    } else {
+      alert(`Invalid input. Please enter a valid integer.`)
+    }
+  })
+
+  const rowTemplate = document.getElementById('row-template')
+  const cellTemplate = document.getElementById('cell-template')
+}
