8d60c159e134ebcab3fff3910dfd3c509ebc5f86
codingleaf <155798178+codingleaf@users.noreply.github.com>
2025-09-03 11:21:45 +0800

    feat: implement increasing opacity effect

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,18 +2,52 @@ document.addEventListener('DOMContentLoaded', () => {
   main()
 })
 
-function randColor() {
-  const colors = [
-    '#FF0000',  // red
-    '#FF7F00',  // orange
-    '#FFFF00',  // yellow
-    '#00FF00',  // green
-    '#0000FF',  // blue
-    '#4B0082',  // indigo
-    '#8B00FF',  // violet
-  ]
-  const randIdx = Math.trunc(Math.random() * 7)
-  return colors[randIdx]
+function main () {
+  const gridSizeForm = document.getElementById('grid-size')
+  const gridSizeInput = document.getElementById('grid-size-input')
+  const gridNode = document.querySelector('.grid')
+  const grid = generateGrid(gridNode)
+
+  gridSizeForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const size = Number(gridSizeInput.value)
+    if (Number.isInteger(size) && size > 0 && size <= 100) {
+      grid.generate(size)
+    } else {
+      alert('Invalid input. Please enter a valid integer. Min: 1 Max: 100')
+    }
+  })
+
+  grid.node.addEventListener('mouseover', (e) => {
+    if (e.target.matches('.cell')) {
+      const cell = e.target
+      if (cell.style.backgroundColor) {
+        const cellOpacity = parseFloat(cell.style.opacity)
+        cell.style.opacity =  Math.min(cellOpacity + 0.1, 1)
+      } else {
+        cell.style.backgroundColor = randColor(COLORS)
+        cell.style.opacity = '0.1'
+      }
+    }
+  })
+}
+
+const COLORS = [
+  '#FF0000', // red 
+  '#FF7F00', // orange 
+  '#FFFF00', // yellow 
+  '#00FF00', // green 
+  '#0000FF', // blue 
+  '#4B0082', // indigo 
+  '#8B00FF', // violet
+]
+
+function randColor(colors = null) {
+  if (colors) {
+    const randIdx = Math.trunc(Math.random() * colors.length)
+    return colors[randIdx]
+  }
+  return '#000000'
 }
 
 function generateGrid(gridNode) {
@@ -21,12 +55,6 @@ function generateGrid(gridNode) {
     node: gridNode,
     generate: (rows, cols) => {
       return generate(rows, cols)
-    },
-    get width() {
-      return getGridWidth(this.node)
-    },
-    get height() {
-      return getGridHeight(this.node)
     },
   }
 
@@ -38,7 +66,7 @@ function generateGrid(gridNode) {
     const contentWidth = clientWidth - paddingLeft - paddingRight
     return contentWidth
   }
-
+  
   function getGridHeight() {
     const clientHeight = grid.node.clientHeight
     const style = getComputedStyle(grid.node)
@@ -77,32 +105,9 @@ function generateGrid(gridNode) {
       gridFragment.appendChild(gridRow)
     }
 
-    // Add rows to grid
+    // Add rows to the grid
     grid.node.appendChild(gridFragment)
   }
 
   return grid
-}
-
-function main () {
-  const gridSizeForm = document.getElementById('grid-size')
-  const gridSizeInput = document.getElementById('grid-size-input')
-  const gridNode = document.querySelector('.grid')
-  const grid = generateGrid(gridNode)
-
-  gridSizeForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const size = Number(gridSizeInput.value)
-    if (Number.isInteger(size) && size > 0) {
-      grid.generate(size)
-    } else {
-      alert(`Invalid input. Please enter a valid integer.`)
-    }
-  })
-
-  grid.node.addEventListener('mouseover', (e) => {
-    if (e.target.matches('.cell')) {
-      e.target.style.backgroundColor = randColor()
-    }
-  })
 }
